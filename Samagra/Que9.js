502271f62ab437b6a41f21f8a1cc25477bd0af21
Samagra2001 <samagrarajoria20011709@gmail.com>
2025-01-16 19:00:19 +0530

    First

---
+++ Samagra/Que9.js
@@ -0,0 +1,8 @@
+//Double all
+
+let a=2;
+let b=3;
+let c=4;
+let d=5;
+let e=1;
+console.log((a+b+c+d+e)*2);
--- d1.js
+++ d1.js
@@ -1,6 +1,9 @@
 // console.log("Hellow World");
 
 let x = 10; 
+
 console.log(x);
 
 console.log(x*2);
+
+
