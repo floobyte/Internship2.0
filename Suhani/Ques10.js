d15175bb5d7a678e85c7c0712dbd63f0051ba565
Suhani546 <Suhanijain551@gmail.com>
2025-01-16 19:34:21 +0530

    This is my day1

---
+++ Suhani/Ques10.js
@@ -0,0 +1,3 @@
+// Infinity Stones
+let sum_2 = one + two*2 + three*3 + four*3 + five*2 + six*2;
+console.log(sum_2);
---
+++ Suhani/Ques7.js
@@ -0,0 +1,5 @@
+// Find X
+let X = 3;
+let newValue = X * 3;
+newValue += 10;
+console.log(newValue);
