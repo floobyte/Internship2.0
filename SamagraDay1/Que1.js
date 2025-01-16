a40cca8807006abbfa6aae698f2645dd528c93f1
Samagra2001 <samagrarajoria20011709@gmail.com>
2025-01-16 19:25:38 +0530

    first commit

---
+++ SamagraDay1/Que1.js
@@ -0,0 +1,9 @@
+//Print Floobyte School
+
+
+
+
+let print="Floobyte School";
+let print1="A Transformation in Education";
+console.log("Floobyte School");
+console.log("A Transformation in Education");
---
+++ SamagraDay1/Que6.js
@@ -0,0 +1,8 @@
+// Square and Sum
+
+
+
+let first=2;
+let second=3;
+let thirnd=4;
+console.log(a*a+b*b+c*c);
