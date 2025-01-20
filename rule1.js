c78b9e53f0e718894a9d6d8a2d330f4390dedf99
vikas-011 <vikas.tech011@gmail.com>
2025-01-20 10:50:46 +0530

    operators Class

--- rule1.js
+++ rule1.js
@@ -1,8 +1,50 @@
-console.log("Hello World");
+// // console.log("Hello World");
 
-// 1. Let  a = 10 ; Wrong - Right
-// 2. let 123gh = 10; Wrong - Right
-// 3. let ghl123 = 10; Wrong - Right
-// 4. let abc123gh = 10; Wrong - Right
-// 5. let Let = 10; Wrong - Right  = Wrong Way
-// 6. let abc 123 = 10; Wrong - Right
+// // 1. Let  a = 10 ; Wrong - Right
+// // 2. let 123gh = 10; Wrong - Right
+// // 3. let ghl123 = 10; Wrong - Right
+// // 4. let abc123gh = 10; Wrong - Right
+// // 5. let Let = 10; Wrong - Right  = Wrong Way
+// // 6. let abc 123 = 10; Wrong - Right
+
+
+
+// // console.log(0/0); // Not a number
+
+// // console.log(Math.sqrt(-1)); 
+
+// console.log("abc" * 10); // NaN
+// console.log("123a" - 0); // NaN
+
+// // let x;
+// // console.log(x + 2); // NaN
+
+// // function add(a, b) {
+// //   return a + b;
+// // }
+// // console.log(add(5)); // NaN (b is undefined)
+// console.log(parseInt("hello")); // NaN
+// console.log(parseFloat("123abc")); // NaN
+// console.log(Math.log(-1)); // NaN
+
+
+
+// console.log(isNaN("abc"));
+// console.log(isNaN(123));
+
+// let l = NaN;
+// console.log(l); // NaN
+
+
+
+
+// console.log(Math.floor(4.7)); // 4 
+// console.log(Math.floor(4.2)); // 4
+// console.log(Math.floor(-4.7)); // -5
+// console.log(Math.floor(-4.2)); // -5
+
+
+console.log(Math.ceil(4.7)); // 5
+console.log(Math.ceil(4.2)); // 5
+console.log(Math.ceil(-4.7)); // -4
+console.log(Math.ceil(-4.2)); // -4
